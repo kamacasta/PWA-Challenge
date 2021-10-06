de59f746505e1b826a0ba8fc573e0cb4e5fdcf67
kama casta <kamacasta983@gmail.com>
2021-10-06 16:02:03 -0700

    added variables to the uploadedtransaction function still working on logic

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,13 +6,13 @@ let db;
 // function stores all the upgrades pending and increments
 request.onupgradeneeded = e => {
     db = e.target.result;
-    db.createObjectStore("pending_transaction", { autoIncrement: true });
+    db.createObjectStore("new_transaction_pending", { autoIncrement: true });
 };
-// function updates the transaction once connection is made
+// function uploads the transaction once connection is made
 request.onsuccess = e => {
     db = e.target.result
     if (navigator.onLine) {
-        updateTransactions();
+        updloadTransactions();
     }
 };
 //  function created to log any errors
@@ -22,7 +22,20 @@ request.onerror = e => {
 
 // function for when the user is offline they can view all transactions that were stored once the user had no internet connection
 function saveRecord(record) {
-    const transaction = db.transaction(["pending_transaction"], "readwrite");
-    const storedTransactions = transaction.objectStore("pending_transaction");
+    const transaction = db.transaction(["new_transaction_pending"], "readwrite");
+    const storedTransactions = transaction.objectStore("new_transaction_pending");
     storedTransactions.add(record);
 };
+
+// function updates all the transactions that were saved by the user
+function updloadTransactions() {
+
+// pulls the transaction from the database
+const transaction = db.transaction(["new_transaction_pending"], "readwrite")
+
+// variable created to access the stored object (transaction)
+const transactionStored = transaction.objectStore("new_transaction_pending")
+// variable created to grab all the records and by doing so will set them to a variable.
+const getAll = transactionObjectStore.getAll()
+
+}
